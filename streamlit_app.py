26ba8ae63d808df065ef170dd7b04ff171693b8f
KAKY SUZY Joelly <165904554+SuzyJoelly@users.noreply.github.com>
2025-03-13 18:06:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 ML App")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "This app builds a machine learning model."
 )
