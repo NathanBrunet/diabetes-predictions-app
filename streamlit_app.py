6ff6132636bbbcb39ffb6d23e969c55077699ef4
KAKY SUZY Joelly <165904554+SuzyJoelly@users.noreply.github.com>
2025-03-14 01:11:07 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd 
 
 st.title("🎈 ML App")
-st.write(
+st.info(
     "This app builds a machine learning model."
 )
-df = pd.read_csv('
+with st.expander ('Data'):
+   st.write ('**Raw data**')  
+ df = pd.read_csv('https://raw.githubusercontent.com/SuzyJoelly/diabetes-predictions-app/refs/heads/main/convertcsv.md')
+
